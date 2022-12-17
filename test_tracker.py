c25b413807457deaa3483c2103ac63e4bccf96e7
Katie Vogel <kvog983@gmail.com>
2022-12-17 10:27:29 -0500

    wrote unittest for query_account_balance
    check that expences, income, profit exist. test that profit is equal to income minus expense

--- test_tracker.py
+++ test_tracker.py
@@ -14,7 +14,7 @@ class TestBudgetTracker(unittest.TestCase):
     
     def test_query_all_txns(self):
         '''
-        
+        Tests that data (keys and values) are returned and not non-existent
         '''
         #item_name = mock.item_name
         txs = query_all_transactions()
@@ -23,9 +23,20 @@ class TestBudgetTracker(unittest.TestCase):
         self.assertTrue(tx['transaction_date'] is not None)
         self.assertTrue("transaction_date" in tx.keys())
         self.assertTrue(['transaction_date', 'item_name', 'item_category', 'unit_cost', 'total_units', 'total_cost'] == list(tx.keys()))
-
-
+    
+    def test_query_account_balance(self):
+        '''
+        Tests that the account balance is calculated and returned
+        '''
+        expenses, income, profit = query_account_balance()
+        self.assertTrue(expenses is not None)
+        self.assertTrue(income is not None)
+        self.assertTrue(profit is not None)
+        self.assertEqual(profit, (income - expenses))
         
+        # next: mocks
+
+    
     def test_computation(self):
         self.assertEqual(2, 1+1)
         self.assertEqual(6, reduce(lambda acc, x: acc + x, [1, 2, 3]))
