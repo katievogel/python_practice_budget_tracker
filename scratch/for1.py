5481a88722c57cc45e0ea6337d64152d94089282
Katie Vogel <kvog983@gmail.com>
2023-01-08 10:31:58 -0500

    Formatted class to start at beginning of file each time, no extra records
    Formatted the class to always start at the beginning of the file when read more than once, and without created an additional record due to duplicating the read of the headers. Also cached the lofd, removed the duration stuff

--- scratch/for1.py
+++ scratch/for1.py
@@ -56,6 +56,7 @@ def make_list_of_dict_from_csvfile(file):
 
 class KatieCSVDictReaderClass():
     headers = None
+    l_of_d = None
     def __init__(self, file):
         self.file = file
         # start = time.time()
@@ -64,28 +65,18 @@ class KatieCSVDictReaderClass():
         # print(self.duration)
 
     def read_headers(self):
-        start = time.time()
         if not self.headers:
             self.headers = read_csv_headers(self.file)
-        print(self.headers)
-        end = time.time()
-        self.duration = end - start
-        print(self.duration)
         return self.headers
 
     def read_records(self):
-        start = time.time()
         self.headers = self.read_headers()
-        self.body_lines = self.file.readlines()
-        self.records = [line.strip('\n').split(',') for line in self.body_lines]
-        self.l_of_d = [dict(zip(self.headers, r)) for r in self.records]
-        end = time.time()
-        self.duration = end - start
-        print(self.duration)
-
-
-            
-
+        if not self.l_of_d:
+            body_lines = self.file.readlines()
+            records = [line.strip('\n').split(',') for line in body_lines]
+            self.l_of_d = [dict(zip(self.headers, r)) for r in records]
+        return self.l_of_d
+      
 
 class TestKatieCSVDictReader(unittest.TestCase):
     def setUp(self):
@@ -93,15 +84,21 @@ class TestKatieCSVDictReader(unittest.TestCase):
         self.reader = KatieCSVDictReaderClass(self.file)
     
     def test_katie_csv_file_headers_read(self):
-        headers = self.reader.header
+        headers = self.reader.read_headers()
         self.assertEqual(headers, ['transaction_date', 'item_name', 'item_category', 'unit_cost', 'total_units', 'total_cost'])
     
     def test_katie_csv_file_records_read(self): 
-        records = self.reader.records
+        records = self.reader.read_records()
         self.assertTrue(len(records) > 0)
 
+    def test_katie_csv_file_read_records_again(self):
+        records = self.reader.read_records()
+        records_2 = self.reader.read_records()
+        self.assertNotEqual(records_2, [])
+        self.assertEqual(records, records_2)
+
     def test_katie_csv_file_transaction_date(self):
-        transaction_date = self.reader.l_of_d[0]['transaction_date']
+        transaction_date = self.reader.read_records()[0]['transaction_date']
         self.assertEqual(transaction_date, '2022-08-24 00:00:00')
 
     def tearDown(self):
