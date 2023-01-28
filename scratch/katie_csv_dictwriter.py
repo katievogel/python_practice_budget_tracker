9ff348178ea807640e492dc0d2846cfcee3588ac
Katie Vogel <kvog983@gmail.com>
2023-01-28 12:20:33 -0500

    wrote initial header writer function

--- scratch/katie_csv_dictwriter.py
+++ scratch/katie_csv_dictwriter.py
@@ -1,4 +1,13 @@
 import csv
 import unittest
+import os
 
 headers = ['transaction_date', 'item_name', 'item_category', 'unit_cost', 'total_units', 'total_cost' ]
+
+filepath = "./new_transactions.csv"
+
+def write_csv_headers(headers):
+    if not os.path.exists(filepath):
+        file = open(filepath, 'w')
+        writer = csv.writer(file)
+        writer.writerow(headers)
