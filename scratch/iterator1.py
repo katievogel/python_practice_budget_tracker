c6526b61a7eac1723bf701160e5501fc10d63fdf
Katie Vogel <kvog983@gmail.com>
2023-02-18 08:43:56 -0500

    understanding iterators

--- scratch/iterator1.py
+++ scratch/iterator1.py
@@ -26,46 +26,30 @@ class BackwardsRangeIterator():
         else:
             raise StopIteration
 
-class KRange:
+class KRange():
     def __init__(self, current, stop):
         self.current = current
         self.stop = stop
     
     def __iter__(self): return ForwardRangeIterator(self.current, self.stop)
-    def __reversed__(self): return BackwardsRangeIterator(self.current, self.stop)
+    def __reversed__(self): return BackwardsRangeIterator(self.stop, self.current)
 
 
 
 
-def dassert(a, b):
-    if a == b: print("pass")
-    else: print(f"fail: {a}")
+# def dassert(a, b):
+#     if a == b: print("pass")
+#     else: print(f"fail: {a}")
 
-print("running tests")
-dassert([x for x in KRange(0, 3)], [0, 1, 2])
-dassert([x for x in reversed(KRange(0, 3))], [2, 1, 0])
+# print("running tests")
+# dassert([x for x in KRange(0, 3)], [0, 1, 2])
+# dassert([x for x in reversed(KRange(0, 3))], [2, 1, 0])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def katie_list(iter):
-    acc = []
-    try:
-        while True:
-            acc.append(next(iter))
-    except StopIteration:
-        pass
-    return acc
+# def katie_list(iter):
+#     acc = []
+#     try:
+#         while True:
+#             acc.append(next(iter))
+#     except StopIteration:
+#         pass
+#     return acc
